0bf8fe361596158b6ec433a4296a0a2f3f401bb8
Sohail Siadatnejad <sohale@gmail.com>
2024-05-20 23:19:11 +0100

    after split, separate: Timeline.js, TimelineContainer.js and index.js

---
+++ timeline-react/pages/TimelineContainer.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import * as d3 from 'd3';
+import Timeline from './index'; // assuming the Timeline component is in index.js
+
+const TimelineContainer = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    d3.csv("/civilizations-timeline.csv").then(parsedData => {
+      const formattedData = parsedData.map(d => ({
+        ...d,
+        start: +d.start,
+        end: +d.end
+      }));
+      setData(formattedData);
+    });
+  }, []);
+
+  return (
+    <div>
+      {data.length > 0 ? (
+        <Timeline data={data} />
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+};
+
+export default TimelineContainer;
---
+++ timeline-react/pages/index.js
@@ -0,0 +1,14 @@
+
+// How to provide data from csv file timeline-react/public/civilizations-timeline.csv
+
+import TimelineContainer from './TimelineContainer';
+
+const HomePage = () => {
+  return (
+    <div>
+      <TimelineContainer />
+    </div>
+  );
+};
+
+export default HomePage;
